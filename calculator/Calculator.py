a4583b3fa0168af8a7e565ea8d9dd36173294199
noelxp <Noel@clix.com>
2023-09-07 16:32:29 -0500

    Testing out adding changes with VS code. Added basic functions"

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -1,3 +1,17 @@
+# A simple calculator program to pass unit tests in calcTests.py
 
-class TestCalculator():
+class Calculator:
+    def __init__(self):
+        pass
 
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a-b
+
+    def multiply(self, a, b):
+        return a*b
+
+    def divide(self, a, b):
+        return a/b
