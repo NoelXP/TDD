4e33000e8085f51f72f1f9bb726ebe7bc8d00b15
noelxp <Noel@clix.com>
2023-09-09 11:01:43 -0500

    added tests.sh script

---
+++ tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python3 /poker/deck_of_cards/deckTests.py
+python3 /poker/deck_of_cards/cardTests.py
+
+
+
+
+
+
+###
+python3 /poker/deck_of_cards/handTests.py
+python3 /poker/deck_of_cards/playerTests.py
+python3 /poker/deck_of_cards/gameTests.py
+python3 /poker/deck_of_cards/pokerTests.py
+python3 /poker/deck_of_cards/roundTests.py
+###
